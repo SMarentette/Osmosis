6d78a1d9108f4114d54b93cb989c33ea68fc6c62
Steve Marentette <steve.marentette@cima.ca>
2025-11-11 08:54:28 -0800

    Removed random .osm file, fixed pep8 lint warnings

--- tests/test_space_types.py
+++ tests/test_space_types.py
@@ -9,21 +9,21 @@ def test_space_type_load():
     path = os.path.join(base, "data", "NECB2017_space_types.osm")
     necb_templates = osmo.Model.load(path)
     assert len(necb_templates.spaceTypes) > 0
-    
+
     # Get a random template spacetype
     template_space_type = necb_templates.space_types[5]
     assert template_space_type.name is not None
-    
+
     # Load a test model
     base = os.path.dirname(__file__)
     path = os.path.join(base, "data", "Model.osm")
     path_out = os.path.join(base, "data", "Model_out.osm")
     model = osmo.Model.load(path)
     assert len(model.spaces) > 0
-    
+
     # Add spacetype to model
     model_space_type = model.add_space_type(template=template_space_type)
-    
+
     # Set the space type on a space
     space = model.spaces[0]
     space.set_space_type(model_space_type)
@@ -32,4 +32,3 @@ def test_space_type_load():
     assert space.space_type is not None
     assert space.space_type.name == template_space_type.name
     model.save(path_out)
-
